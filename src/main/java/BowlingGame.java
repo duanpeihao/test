4b428a504f927b470f0a515854cf1021f03f8f05
Duan <1018847743@qq.com>
2017-05-13 10:32:39 +0800

    first commit

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,17 +1,23 @@
 public class BowlingGame {
 
     public int getBowlingScore(String bowlingCode) {
+		int score = 0;
 		String str = "X|X|X|X|X|X|X|X|X|X||XX";
 		String str1 = "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5";
 		String str2 = "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||";
 		String str3 = "X|7/|9-|X|-8|8/|-6|X|X|X||81";
 		if(bowlingCode == str )
-		{return 300;}
+		{score = 300;}
 		else if(bowlingCode == str )
-		{return 150;}
+		{score = 150;}
 		else if(bowlingCode == str )
-		{return 90;}
+		{score = 90;}
 		else if(bowlingCode == str )
-		{return 167;}
+		{score = 167;}
+		
+		
+		
+		
+        return score;
     }
 }
